a7893669ba68cad99d3e2c9ef659a77c7606c6bf
gabeaustin <gabriel.austin2019@gmail.com>
2021-09-21 19:42:32 -0500

    updated readme

--- src/index.js
+++ src/index.js
@@ -4,10 +4,4 @@ import "./index.css";
 import "semantic-ui-css/semantic.min.css";
 import App from "./App";
 
-import { Button } from 'semantic-ui-react'
-
-ReactDOM.render(
-    <App />,
-  document.getElementById("root")
-);
-
+ReactDOM.render(<App />, document.getElementById("root"));
