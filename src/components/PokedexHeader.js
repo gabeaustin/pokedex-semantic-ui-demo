e33da93141318659b0a6c21961b2cd78ae90acf2
gabeaustin <gabriel.austin2019@gmail.com>
2021-09-21 21:07:26 -0500

    added semantic header

--- src/components/PokedexHeader.js
+++ src/components/PokedexHeader.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Segment } from "semantic-ui-react";
+import { Header } from "semantic-ui-react";
 
 const PokedexHeader = ({ name }) => {
   return (
     <Segment basic inverted padded="very" vertical>
-      <h1>This is a Header for {name} Pokedex!</h1>
-      <h3>This is a subheading.</h3>
+      <Header as="h1">This is a Header for {name} Pokedex!</Header>
+      <Header as="h3">This is a subheading.</Header>
     </Segment>
   );
 };
