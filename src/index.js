3157054098161b0088f574113a80c3e2b890623c
gabeaustin <gabriel.austin2019@gmail.com>
2021-09-21 19:39:08 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "semantic-ui-css/semantic.min.css";
+import App from "./App";
+
+import { Button } from 'semantic-ui-react'
+
+ReactDOM.render(
+    <App />,
+  document.getElementById("root")
+);
+
