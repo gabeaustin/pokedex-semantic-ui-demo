5f3dca4e58081133bda018ab52b708b88455c570
gabeaustin <gabriel.austin2019@gmail.com>
2021-09-21 20:55:15 -0500

    added the segment prop for the header

--- src/components/PokedexHeader.js
+++ src/components/PokedexHeader.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Segment } from "semantic-ui-react";
 
 const PokedexHeader = ({ name }) => {
-  return <h1>This is a Header for {name} Pokedex!</h1>;
+  return (
+    <Segment basic inverted padded="very" vertical>
+      <h1>This is a Header for {name} Pokedex!</h1>
+      <h3>This is a subheading.</h3>
+    </Segment>
+  );
 };
 
 export default PokedexHeader;
